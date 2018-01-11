43b9e80f1d180de1b51941eba7d5d53f2e408f50
lspCoder <544295975@qq.com>
2018-01-11 22:29:28 +0800

    js图片懒加载

--- js-lazyload/assets/js/lazyload.js
+++ js-lazyload/assets/js/lazyload.js
@@ -21,16 +21,45 @@
      * @param {*回调函数} fn 
      * @param {*延迟时间} delay 
      */
-    var throttle = function (fn, delay) {
+    // var throttle = function (fn, delay) {
+    //     var timer = null;
+    //     return function () {
+    //         var context = this;
+    //         clearTimeout(timer);
+    //         timer = setTimeout(function () {
+    //             fn.apply(context, arguments);
+    //         }, delay);
+    //     }
+    // }
+
+    /**
+     * 函数节流方法
+     * @param Function fn 延时调用函数
+     * @param Number delay 延迟多长时间
+     * @param Number atleast 至少多长时间触发一次
+     * @return Function 延迟执行的方法
+     */
+    var throttle = function (fn, delay, atleast) {
         var timer = null;
+        var previous = null;
+
         return function () {
-            var context = this;
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                fn.apply(context, arguments);
-            }, delay);
+            var now = +new Date();
+
+            if (!previous) previous = now;
+
+            if (now - previous > atleast) {
+                fn();
+                // 重置上一次开始时间为本次结束时间
+                previous = now;
+            } else {
+                clearTimeout(timer);
+                timer = setTimeout(function () {
+                    fn();
+                }, delay);
+            }
         }
-    }
+    };
 
     /* 默认选项 */
     var defaultSetting = {
